3e473536da33f09fa31c016e69f527bbb91e4ec8
Carlos Diaz <cadgo.root@gmail.com>
2022-03-10 13:16:55 -0600

    more code

--- docker/nginx-cd/monitor_nginx_config.sh
+++ docker/nginx-cd/monitor_nginx_config.sh
@@ -1,36 +1,104 @@
 #!/bin/bash
 
-DEFAULT_TIME=300
+DEFAULT_TIME=10
 FILE=""
 FILE_MD5=""
 LOOP=True
+COMMAND=nginx
+NGINX_CONF=/etc/nginx/conf.d/defualt.conf
+RET=""
+DEFAULT_FILTER="kind=appsec"
+
+function nginx_exists(){
+if ! command -v nginx &> /dev/null
+  then
+  echo "$COMMAND needs to exists to validate the file"
+  exit 1
+  fi
+}
+
+function filemd5(){
+  echo $(md5sum $1 | awk '{print $1}')
+}
+
+function nginx_valid_conf(){
+  if ! nginx -t &> /dev/null
+  then
+    echo "Error Validating file by $COMMAND"
+    RET=1
+  else
+    echo "File validation suscess!!!"
+    RET=0
+  fi
+}
+
+function docker_filter_list(){
+  Filter=$1
+  listId=$(curl --unix-socket /var/run/docker.sock --silent -H "Content-type: application/json" -gG -X GET http://localhost/v1.41/containers/json --data-urlencode 'filters={"label":["'$Filter'"]}'| jq '.[].Id')
+  if [[ $(echo "${listId[@]}" | wc -w) == "0" ]]
+  then
+    echo ""
+  else
+    echo $listId
+  fi
+}
+
+function put_file_in_docker(){
+  container=$1
+  echo "container name $container"
+  tmp_file=$(mktemp -u)
+  TARFILE="$tmp_file.$container.tar"
+  echo "tar file $TARFILE"
+  tar -cf "$TARFILE" "$FILE"
+  #curl --unix-socket /var/run/docker.sock -H "Content-Type: application/json" -X PUT "http://localhost/v1.41/containers/769cb917cfe9/archive?path=$NGINX_CONF --data-binary \"@$FILE\""
+}
 
 while getopts "t:f:" options; do
   case "${options}" in
   t)
     DEFAULT_TIME=${OPTARG}
-  ;;
-  f)
-    FILE=${OPTARG}
+  ;; f) FILE=${OPTARG}
   ;;
   esac
 done
 
-if [ -n "$FILE" ]
+##-------- Validation Stuff
+if [ -z "$FILE" ]
 then
  echo "File is needed to continue"
  exit 0
 fi
-
+nginx_exists
+dockerList=$(docker_filter_list "$DEFAULT_FILTER")
+for cont in $dockerList
+do
+  echo "put file in $cont"
+  put_file_in_docker $cont
+done
+#------ End of validation Stuff
 while [ "$LOOP" ]
-do 
-  if [ -n "$FILE_MD5" ] 
+do
+  if [ -z "$FILE_MD5" ]
   then
-    echo "No existe MD5"
-    FILE_MD5=$(md5sum $FILE | awk '{print $1}')
-    echo $FILE_MD5
+    echo "first time running obtaining md5"
+    nginx_valid_conf
+    if [ "$RET" == "0" ]; then
+      FILE_MD5=$(filemd5 $FILE)
+      #echo $FILE_MD5
+    fi
   else
-    echo "Si Existe MD5"
+    echo "asdasdas"
+    NEW_FILE=$(filemd5 $FILE)
+    if [ "$NEW_FILE" != "$FILE_MD5" ]
+    then
+      echo "No son iguales"
+      #1 Validar el archivo en el contenedor
+      nginx_valid_config
+      #2 Si es valido se actualiza el nuevo md5
+      FILE_MD5=$NEW_FILE
+      #3 si no es valido se descarta y se mantiene el Md5 valido
+    fi
   fi
   sleep $DEFAULT_TIME
 done
+
